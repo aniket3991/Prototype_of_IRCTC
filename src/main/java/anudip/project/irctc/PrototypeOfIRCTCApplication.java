79b8d73d2ae7e3e004fb5b84452afd5092b9e693
Aniket Mishra <3991aniketmishra@gmail.com>
2024-03-04 20:49:05 +0530

    pushing project structure

---
+++ src/main/java/anudip/project/irctc/PrototypeOfIRCTCApplication.java
@@ -0,0 +1,13 @@
+package anudip.project.irctc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeOfIRCTCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeOfIRCTCApplication.class, args);
+	}
+
+}
---
+++ src/test/java/anudip/project/irctc/PrototypeOfIrctcApplicationTests.java
@@ -0,0 +1,13 @@
+package anudip.project.irctc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeOfIrctcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
